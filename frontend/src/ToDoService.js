79579bde0c895ff9c7d91dca89653d349b631105
Alexander Nechaev <alex26g4@hotmail.com>
2020-11-15 17:59:39 +0200

    Updates 15.11.2020

---
+++ frontend/src/ToDoService.js
@@ -0,0 +1,8 @@
+class ToDoService {
+  
+  AddToDo(item) {
+    console.log(item.id +" "+ item.text +" "+ item.complete)
+  }
+}
+
+export default ToDoService;
--- frontend/src/components/ToDoItem/Todo.jsx
+++ frontend/src/components/ToDoItem/Todo.jsx
@@ -5,15 +5,21 @@ import Checkbox from "@material-ui/core/Checkbox";
 
 export default class Todo extends React.Component {
   render() {
+    const checkBox = (
+      <Checkbox
+        checked={this.props.item.complete}
+        inputProps={{ "aria-label": "Checkbox A" }}
+        onClick={this.props.toggleComplete}
+      />
+    );
+
+    const deleteBtn = <Delete onClick={this.props.onDelete} />;
+
     return (
       <div className="itemContainer">
-        <Checkbox
-          checked={this.props.item.complete}
-          inputProps={{ "aria-label": "Checkbox A" }}
-          onClick={this.props.toggleComplete}
-        />
+        {checkBox}
         <div>{this.props.item.text}</div>
-        <Delete onClick={this.props.onDelete} />
+        {deleteBtn}
       </div>
     );
   }
