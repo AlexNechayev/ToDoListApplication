e7f3a1c77d2778499a7287a92f216de824f5b484
Alexander Nechaev <alex26g4@hotmail.com>
2020-11-20 18:15:45 +0200

    ToDoService init

--- frontend/src/ToDoService.js
+++ frontend/src/ToDoService.js
@@ -1,8 +1,58 @@
-class ToDoService {
-  
-  AddToDo(item) {
-    console.log(item.id +" "+ item.text +" "+ item.complete)
+export default class ToDoService {
+
+  static instance = null;
+
+  static getInstance(){
+    if(ToDoService.instance == null){
+      ToDoService.instance = new ToDoService();
+    }
+    return this.instance;
   }
+    
+  AddToDo(item) {
+    try{
+       //Backend: Post item
+      console.log(
+        "Service called with item details: " +
+          item.id +
+          " " +
+          item.text +
+          " " +
+          item.complete
+      );
+    }catch(error){
+      console.log(error);
+    }
+
+  }
+
+  getAllItems(){
+    items = [];
+    try{
+      //Initialization of the item list;
+      return list
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  removeItem(itemID){
+    try{
+      //find the given id in the database and erase it from the list.
+    }catch(error){
+      console.log(error);
+    } 
+  }
+
+  toggleItemComplete(itemID){
+    try{
+      //find the given id in the database then toggle it's state.
+      return list
+    }catch(error){
+      console.log(error);
+    }
+  }
+
 }
 
 export default ToDoService;
